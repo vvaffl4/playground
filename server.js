3b9cb600760762b05fb1a4d4e5f6377684e52bb8
PW\pw <patrickwobben@live.nl>
2022-10-18 00:33:16 +0200

    Db connect

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const { Sequelize } = requrie('sequelize');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const sequelize = new Sequelize('playground', 'user', 'ThisIsAPassword1!', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+
+app.get('/', async (req, res) => {
+  
+  try {
+    await sequelize.authenticate();
+    res.send('Hello Happy World!');
+  } catch (error) {
+    res.send(error);
+  }
 })
 
 app.listen(port, () => {
