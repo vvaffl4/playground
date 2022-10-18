ba6ef0dbccb50b4d92fcfe52fbdbc1badc3b6847
PW\pw <patrickwobben@live.nl>
2022-10-18 02:07:21 +0200

    a

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Sequelize } = requrie('sequelize');
 const app = express()
 const port = 3000
 
-const sequelize = new Sequelize('playground', 'user', 'ThisIsAPassword1!', {
+const sequelize = new Sequelize('playground', 'vvaffl4', 'ThisIsAPassword1!', {
   host: 'mysql:3306',
   dialect: 'mysql'
 });
